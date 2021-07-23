5b6fb2281c9f783790cee8912ea499d48463e440
Yann Bolliger <yann.bolliger@epfl.ch>
2021-07-23 15:43:09 +0200

    Write about PeerList.

---
+++ thesis-report/src/code/lib.rs
@@ -0,0 +1,151 @@
+//! Provides a peer list for use within the `Supervisor`
+
+extern crate stainless;
+use stainless::*;
+
+mod list;
+use list::*;
+
+/// Node IDs
+// PeerId was replaced by a simple u128 to make hashing easier.
+
+pub enum ErrorKind {
+    NoWitnessLeft { context: Option<Box<ErrorKind>> },
+}
+
+/// A generic container mapping `u128`s to some type `T`,
+/// which keeps track of the primary peer, witnesses, full nodes,
+/// and faulty nodes. Provides lifecycle methods to swap the primary,
+/// mark witnesses as faulty, and maintains an `invariant` for
+/// correctness.
+#[derive(Clone)]
+pub struct PeerList<T> {
+    values: ListMap<u128, T>,
+    primary: u128,
+    witnesses: ListSet<u128>,
+    full_nodes: ListSet<u128>,
+    faulty_nodes: ListSet<u128>,
+}
+
+impl<T> PeerList<T> {
+    /// Invariant maintained by a `PeerList`
+    ///
+    /// ## Implements
+    /// - [LCD-INV-NODES]
+    pub fn invariant(peer_list: &PeerList<T>) -> bool {
+        peer_list.full_nodes.is_disjoint(&peer_list.witnesses)
+            && peer_list.full_nodes.is_disjoint(&peer_list.faulty_nodes)
+            && peer_list.witnesses.is_disjoint(&peer_list.faulty_nodes)
+            && !peer_list.witnesses.contains(&peer_list.primary)
+            && !peer_list.full_nodes.contains(&peer_list.primary)
+            && !peer_list.faulty_nodes.contains(&peer_list.primary)
+            && peer_list.values.contains(&peer_list.primary)
+            && peer_list.values.contains_all(&peer_list.witnesses)
+            && peer_list.values.contains_all(&peer_list.full_nodes)
+            && peer_list.values.contains_all(&peer_list.faulty_nodes)
+    }
+
+    /// Get a reference to the light client instance for the given peer id.
+    pub fn get(&self, peer_id: &u128) -> Option<&T> {
+        self.values.get(peer_id)
+    }
+
+    /// Get current primary peer id.
+    pub fn primary_id(&self) -> u128 {
+        self.primary
+    }
+
+    /// Get a reference to the current primary instance.
+    pub fn primary(&self) -> &T {
+        // SAFETY: Enforced by invariant
+        self.values.get(&self.primary).unwrap()
+    }
+
+    /// Get all the witnesses peer ids
+    pub fn witnesses_ids(&self) -> &ListSet<u128> {
+        &self.witnesses
+    }
+
+    /// Get all the full nodes peer ids
+    pub fn full_nodes_ids(&self) -> &ListSet<u128> {
+        &self.full_nodes
+    }
+
+    /// Get all the faulty nodes peer ids
+    pub fn faulty_nodes_ids(&self) -> &ListSet<u128> {
+        &self.faulty_nodes
+    }
+
+    /// Remove the given peer from the list of witnesses,
+    /// and mark it as faulty. Get a new witness from
+    /// the list of full nodes, if there are any left.
+    /// Returns the new witness, if any.
+    ///
+    /// ## Precondition
+    /// - The given peer id must not be the primary peer id.
+    /// - The given peer must be in the witness list
+    #[pre(
+        Self::invariant(&self)
+        && !(faulty_witness == self.primary)
+        && self.witnesses.contains(&faulty_witness)
+    )]
+    #[post(
+        Self::invariant(&self)
+        && !self.witnesses.contains(&faulty_witness)
+        && self.faulty_nodes.contains(&faulty_witness)
+    )]
+    pub fn replace_faulty_witness(
+        &mut self, faulty_witness: u128
+    ) -> Option<u128> {
+        let mut result = None;
+
+        self.witnesses.remove(&faulty_witness);
+
+        if let Some(new_witness) = self.full_nodes.first() {
+            self.witnesses.insert(new_witness);
+            self.full_nodes.remove(&new_witness);
+            result = Some(new_witness);
+        }
+
+        self.faulty_nodes.insert(faulty_witness);
+        result
+    }
+
+    /// Mark the primary as faulty and swap it for the next available witness,
+    /// if any. Returns the new primary on success.
+    ///
+    /// ## Errors
+    /// - If there are no witness left, returns `ErrorKind::NoWitnessLeft`.
+    #[pre(Self::invariant(&self))]
+    #[post((matches!(ret, Ok(_))).implies(
+        Self::invariant(&self)
+            && old(&self).primary != self.primary
+            && self.faulty_nodes.contains(&old(&self).primary)
+            && old(&self).witnesses.contains(&self.primary)
+    ))]
+    pub fn replace_faulty_primary(
+        &mut self,
+        primary_error: Option<Box<ErrorKind>>,
+    ) -> Result<u128, Box<ErrorKind>> {
+        self.faulty_nodes.insert(self.primary);
+
+        if let Some(new_primary) = self.witnesses.first() {
+            self.primary = new_primary;
+            self.witnesses.remove(&new_primary);
+            Ok(new_primary)
+        } else if let Some(err) = primary_error {
+            Err(Box::new(ErrorKind::NoWitnessLeft { context: Some(err) }))
+        } else {
+            Err(Box::new(ErrorKind::NoWitnessLeft { context: None }))
+        }
+    }
+
+    /// Get a reference to the underlying `HashMap`
+    pub fn values(&self) -> &ListMap<u128, T> {
+        &self.values
+    }
+    /// Consume into the underlying `HashMap`
+    pub fn into_values(self) -> ListMap<u128, T> {
+        self.values
+    }
+}
---
+++ thesis-report/src/code/list.rs
@@ -0,0 +1,119 @@
+use super::*;
+
+#[derive(Clone)]
+pub struct ListSet<T> {
+    list: List<T>,
+}
+#[derive(Clone)]
+pub struct ListMap<K, V> {
+    list: List<(K, V)>,
+}
+
+#[derive(Clone)]
+enum List<T> {
+    Nil,
+    Cons(T, Box<List<T>>),
+}
+
+impl ListSet<u128> {
+    pub fn empty() -> Self {
+        ListSet { list: List::Nil }
+    }
+    pub fn is_disjoint(&self, other: &ListSet<u128>) -> bool {
+        is_equal(
+            &self.list.contents().intersection(other.list.contents()),
+            &Set::new(),
+        )
+    }
+    pub fn contains(&self, t: &u128) -> bool {
+        self.list.contents().contains(&t)
+    }
+
+    #[post(
+      !self.contains(&t)
+      && self.list.contents().is_subset(&old(&self).list.contents())
+    )]
+    pub fn remove(&mut self, t: &u128) {
+        self.list.remove(t);
+    }
+
+    #[post(self.contains(&t))]
+    pub fn insert(&mut self, t: u128) {
+        self.list.insert(t);
+    }
+    pub fn first(&self) -> Option<u128> {
+        match &self.list {
+            List::Cons(t, _) => Some(*t),
+            _ => None,
+        }
+    }
+}
+
+impl<V> ListMap<u128, V> {
+    pub fn get(&self, key: &u128) -> Option<&V> {
+        self.list.get(key)
+    }
+    pub fn contains(&self, key: &u128) -> bool {
+        self.list.key_set().contains(&key)
+    }
+    pub fn contains_all(&self, keys: &ListSet<u128>) -> bool {
+        is_equal(
+            &self.list.key_set().intersection(keys.list.contents()),
+            &keys.list.contents(),
+        )
+    }
+}
+
+fn is_equal<'a>(s1: &Set<&'a u128>, s2: &Set<&'a u128>) -> bool {
+    s1.is_subset(s2) && s2.is_subset(s1)
+}
+
+impl List<u128> {
+    #[measure(self)]
+    pub fn contents(&self) -> Set<&u128> {
+        match self {
+            List::Nil => Set::new(),
+            List::Cons(head, tail) => tail.contents().insert(head),
+        }
+    }
+
+    #[post(
+        !self.contents().contains(&t)
+        && self.contents().is_subset(&old(&self).contents())
+    )]
+    fn remove(&mut self, t: &u128) {
+        let list = std::mem::replace(self, List::Nil);
+        let result = match list {
+            List::Nil => List::Nil,
+            List::Cons(head, mut tail) => {
+                tail.remove(t);
+                if head == *t {
+                    *tail
+                } else {
+                    List::Cons(head, tail)
+                }
+            }
+        };
+        *self = result;
+    }
+    pub fn insert(&mut self, t: u128) {
+        let list = std::mem::replace(self, List::Nil);
+        *self = List::Cons(t, Box::new(list));
+    }
+}
+
+impl<V> List<(u128, V)> {
+    pub fn key_set(&self) -> Set<&u128> {
+        match self {
+            List::Nil => Set::new(),
+            List::Cons(head, tail) => tail.key_set().insert(&head.0),
+        }
+    }
+    pub fn get(&self, key: &u128) -> Option<&V> {
+        match &self {
+            List::Nil => None,
+            List::Cons(head, _) if head.0 == *key => Some(&head.1),
+            List::Cons(_, tail) => tail.get(key),
+        }
+    }
+}
